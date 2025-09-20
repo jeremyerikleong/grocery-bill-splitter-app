d89d5d2327189a389168a0449b3e8c7682857b9b
Jeremy <jeremythegreat95@gmail.com>
2025-09-20 22:35:54 +0800

    add language

--- src/i18n.js
+++ src/i18n.js
@@ -4,10 +4,26 @@ const messages = {
     en: {
         M000001: 'language',
         M000002: 'title',
+        M000003: 'enter the number of participants',
+        M000004: 'customize participant names',
+        M000005: 'number of participants',
+        M000006: 'participant',
+        M000007: 'enter participant name',
+        M000008: 'generate',
+        M000009: 'reset',
+        M000010: 'participants',
     },
     cn: {
         M000001: '语言',
         M000002: '标题',
+        M000003: '输入参与者人数',
+        M000004: '自定义参与者姓名',
+        M000005: '参加人数',
+        M000006: '参与者',
+        M000007: '输入参与者姓名',
+        M000008: '生成',
+        M000009: '重置',
+        M000010: '参与者们',
     },
 }
 
